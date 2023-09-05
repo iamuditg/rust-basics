cd3a142bda79270d51c1fa1c55db7bcf3e021e7f
Udit Gurani <gurani.udit3@gmail.com>
2023-09-05 13:07:22 +0530

    traits

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,20 @@ use crate::control_structure::control_structure;
 use crate::ownership::f_ownership;
 use crate::stack_Imp::stack_implementation;
 use crate::structure::stge_example;
+use crate::traits::traits_ex;
 
 mod basic;
 mod ownership;
 mod control_structure;
 mod stack_Imp;
 mod structure;
+mod traits;
 
 fn main() {
     //basics_rust()
     //f_ownership();
     //control_structure()
     //stack_implementation()
-    stge_example();
+    //stge_example();
+    traits_ex();
 }
