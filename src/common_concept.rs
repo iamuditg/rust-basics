ddc86aa8bf527fa51b7dfb350b950c9b7e8bc723
Udit Gurani <gurani.udit3@gmail.com>
2024-08-19 17:52:06 +0530

    control flow

--- src/common_concept.rs
+++ src/common_concept.rs
@@ -7,6 +7,80 @@ pub fn common_concept() {
     shadowing();
     data_types();
     functions();
+    control_flow();
+}
+
+fn control_flow() {
+    // if expression
+    let number = 3;
+    if number < 5 {
+        println!("condition was true");
+    }else {
+        println!("condition was false");
+    }
+
+    let num = 6;
+    if num %4 == 0 {
+        println!("number is divisible by 4");
+    }else if num % 3 == 0 {
+        println!("number is divisible by 3");
+    }else if num % 2 == 0 {
+        println!("number us divisible by 2");
+    }else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
+
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+    println!("The value of number is: {number}");
+
+
+    // loop
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+        if counter == 10 {
+            break counter * 2
+        }
+    };
+    println!("the result is {result}");
+
+    // loop labels to disambiguate between multiple loops
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up
+            }
+            remaining -= 1;
+        }
+        count += 1;
+    }
+    println!("End count = {count}");
+
+    // conditional loops with while
+    let mut number =3;
+    while number != 0 {
+        println!("number {number}");
+        number -= 1;
+    }
+    println!("LIFTOFF!!");
+
+    let  a = [10,20,30,40,50];
+    for element in a {
+        println!("the value is: {element}");
+    }
+
+    for number in (1..4).rev() {
+        println!("{number}");
+    }
+    println!("LIFTOFF!!!");
 }
 
 fn functions() {
